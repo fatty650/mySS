fa920106ddc0d85554126c24c28b6ed364f1ac76
Layne <Layne@163.com>
2023-02-24 00:24:13 +0800

    first commit

---
+++ src/main/java/com/gdgh/git/GitTest.java
@@ -0,0 +1,13 @@
+package com.gdgh.git;/**
+ * projectName: b2c-store
+ *
+ * @author: 黄俊成
+ * time: 2023/2/24 0:20
+ * version: v1.0.0
+ * description: 测试用
+ */public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello world!");
+
+    }
+}
