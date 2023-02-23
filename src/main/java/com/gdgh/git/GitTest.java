2712ec9ff852d10c4a4e699ec59f987b3734315e
Layne <Layne@163.com>
2023-02-24 00:26:14 +0800

    first commit

--- src/main/java/com/gdgh/git/GitTest.java
+++ src/main/java/com/gdgh/git/GitTest.java
@@ -8,6 +8,6 @@ package com.gdgh.git;/**
  */public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello world!");
-
+        System.out.println("hello world2!");
     }
 }
